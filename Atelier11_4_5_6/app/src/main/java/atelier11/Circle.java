95a47da32308198a068b4b6df7eabdbb64f89ca2
maelie14 <maeliehamel14@outlook.fr>
2023-11-15 17:43:08 -0500

    methods get set circle

--- Atelier11_4_5_6/app/src/main/java/atelier11/Circle.java
+++ Atelier11_4_5_6/app/src/main/java/atelier11/Circle.java
@@ -5,27 +5,32 @@ package atelier11;
 public class Circle extends Shape{
 
     private double radius; //truc de gpt ca marche peut etre pas
-    public void setRadius(double radius){
 
-        this.radius = radius;
-
-    }
-
-    public double getRadius() {
-        return radius;
-
-    }
-    
     public Circle() { } 
 
     public Circle(double radius) { //constructeurs :) si ca shie c la faute a ...
 
-        String isColor;
-        Boolean isFilled;
      }
 
      public Circle(double radius, String isColor, Boolean isFilled ) {
 
         super( isColor, isFilled );
-     }
+    }
+
+    public double getRadius() { //get method
+
+        return radius;
+    }
+    public void setRadius(double radius){ //set method
+
+        this.radius = radius;
+    }
+    public double getArea() { //methods
+        
+        return Math.PI * radius * radius;
+    }
+    public double getPerimeter() {
+        
+        return Math.PI * 2 * radius;
+    }
 }
