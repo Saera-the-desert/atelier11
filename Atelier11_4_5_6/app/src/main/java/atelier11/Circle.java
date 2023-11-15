d09188a900f273a13142f5a02f310d9ceb62745c
maelie14 <maeliehamel14@outlook.fr>
2023-11-15 17:20:38 -0500

    constructeurs circle.java

--- Atelier11_4_5_6/app/src/main/java/atelier11/Circle.java
+++ Atelier11_4_5_6/app/src/main/java/atelier11/Circle.java
@@ -2,7 +2,7 @@ package atelier11;
 
 // class circle
 
-public class Circle{
+public class Circle extends Shape{
 
     private double radius; //truc de gpt ca marche peut etre pas
     public void setRadius(double radius){
@@ -15,4 +15,17 @@ public class Circle{
         return radius;
 
     }
+    
+    public Circle() { } 
+
+    public Circle(double radius) { //constructeurs :) si ca shie c la faute a ...
+
+        String isColor;
+        Boolean isFilled;
+     }
+
+     public Circle(double radius, String isColor, Boolean isFilled ) {
+
+        super( isColor, isFilled );
+     }
 }
